de4d73104b5c4b18c8f8f424d0496bd333766d0e
CallumS <dev@callums005.net>
2024-02-16 20:35:48 +0000

    Server

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,38 +1,167 @@
 ﻿using SFML.Window;
 using SFML.Graphics;
-using System.Reflection;
+using static SFML.Window.Keyboard;
+using static SFML.Window.Mouse;
+using SFML.System;
+using System.Net.Sockets;
+using System.Net;
 
 namespace Client
 {
+    internal class Input
+    {
+        public static bool[] Keys = new bool[101];
+        public static bool[] MouseButtons = new bool[5];
+        public static Tuple<int, int> MousePosition = new Tuple<int, int>(0, 0);
+        public static float ScrollDelta = 0.0f;
+
+        public static bool IsKeyDown(Key keyCode)
+        {
+            return Keys[(int)keyCode];
+        }
+
+        public static bool IsMouseDown(Button button)
+        {
+            return MouseButtons[(int)button];
+        }
+
+        public static Tuple<int, int> GetMousePos()
+        {
+            return MousePosition;
+        }
+
+        public static float GetScrollPos()
+        {
+            return ScrollDelta;
+        }
+    }
+
+    internal class Window
+    {
+        public Window(uint height = 400, string title = "Multiplayer Game")
+        {
+            Height = height;
+            Width = Height * 16 / 9;
+            Title = title;
+
+            _Window = new RenderWindow(new VideoMode(Width, Height), Title);
+            _Window.SetFramerateLimit(60);
+
+            _Window.Closed += (object sender, EventArgs e) =>
+            {
+                _Window.Close();
+            };
+
+            _Window.KeyPressed += (object sender, KeyEventArgs e) => { Input.Keys[(int)e.Code] = true; };
+            _Window.KeyReleased += (object sender, KeyEventArgs e) => { Input.Keys[(int)e.Code] = false; };
+            _Window.MouseButtonPressed += (object sender, MouseButtonEventArgs e) => { Input.MouseButtons[(int)e.Button] = true; };
+            _Window.MouseButtonReleased += (object sender, MouseButtonEventArgs e) => { Input.MouseButtons[(int)e.Button] = false; };
+            _Window.MouseWheelScrolled += (object sender, MouseWheelScrollEventArgs e) => { Input.ScrollDelta = e.Delta; };
+            _Window.MouseMoved += (object sender, MouseMoveEventArgs e) => { Input.MousePosition = new Tuple<int, int>(e.X, e.Y); };
+        }
+
+        public RenderWindow? _Window { get; } = null;
+        public uint Height { get; set; }
+        public string Title { get; }
+        public uint Width { get; }
+        public bool IsOpen 
+        { 
+            get { return _Window.IsOpen; }
+        }
+    }
+
+    internal class Entity
+    {
+        public Entity(string name)
+        {
+            name = Name;
+        }
+
+        public string Name { get; set; }
+        
+        public Vector2f Position { get; set; }
+        public Vector2f Size { get; set; }
+        public Vector2f Velocity { get; set; }
+        public int Rotation { get; set; }
+        public Color Colour { get; set; }
+        public Texture Tex { get; set; }
+        public bool ColourOverTexture { get; set; } = true;
+        public Shape RenderObject { get; set; }
+
+    }
+
+    internal class SocketClient
+    { 
+        public SocketClient()
+        {
+            Start();            
+        }
+
+        async void Start()
+        {
+            IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("localhost");
+            IPAddress ipAddr = ipHostInfo.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 5000);
+
+            clientSocket = new Socket(
+                    ipEndPoint.AddressFamily,
+                    SocketType.Stream,
+                    ProtocolType.Tcp
+                );
+
+            await clientSocket.ConnectAsync(ipEndPoint);
+        }
+
+        async void OnUpdate()
+        {
+
+        }
+
+        void End()
+        {
+            clientSocket.Shutdown(SocketShutdown.Both);
+        }
+
+        Socket clientSocket;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            uint height = 200;
-            uint width = height * 16 / 9;
+            Window window = new Window();
 
-
-            RenderWindow window = new RenderWindow(new VideoMode(width, height), "Multiplayer Game");
-            
-            window.Closed += (object sender, EventArgs e) =>
+            Entity test = new Entity("Test")
             {
-                window.Close();
+                Position = new Vector2f(30, 30),
+                Velocity = new Vector2f(2, 2),
+                Size = new Vector2f(40, 40),
+                Colour = Color.Green,
+            };
+            
+            test.RenderObject = new RectangleShape()
+            {
+                Position = test.Position,
+                Size = test.Size,
+                FillColor = test.Colour,
+                Rotation = test.Rotation
             };
 
-            window.KeyPressed += (object sender, KeyEventArgs e) =>{ };
-            window.KeyReleased += (object sender, KeyEventArgs e) => { };
-            window.MouseButtonPressed += (object sender, MouseButtonEventArgs e) => { };
-            window.MouseButtonReleased += (object sender, MouseButtonEventArgs e) => { };
-            window.MouseWheelScrolled += (object sender, MouseWheelScrollEventArgs e) => { };
-            window.MouseMoved += (object sender, MouseMoveEventArgs e) => { };
 
             while (window.IsOpen)
             {
-                window.DispatchEvents();
+                test.Position += test.Velocity;
+                test.RenderObject.Position = test.Position;
 
-                window.Clear(Color.Red);
-                window.Display();
+                window._Window.DispatchEvents();
+
+                window._Window.Clear(Color.Red);
+
+                window._Window.Draw(test.RenderObject);
+
+                window._Window.Display();
             }
         }
+        
     }
 }
---
+++ Server/Server.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    internal class Server
+    {
+
+        static int PORT = 5000;
+        static IPAddress IP_ADDRESS;
+        static TcpListener _tcpListener;
+
+        static byte[] buffer = new byte[1024];
+        static string? recMsg = null;
+
+        static async void Start()
+        {
+            IP_ADDRESS = IPAddress.Parse("127.0.0.1");
+
+            _tcpListener = new TcpListener(IP_ADDRESS, PORT);
+            _tcpListener.Start();
+
+            using TcpClient client = _tcpListener.AcceptTcpClient();
+
+            NetworkStream tcpStream = client.GetStream();
+            int readTotal;
+
+            while ((readTotal = tcpStream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                recMsg = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Start();
+        }
+    }
+}
